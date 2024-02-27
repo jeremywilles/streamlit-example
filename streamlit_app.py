df08856bc920ded6faddcfef0de192b99f7ce19a
Jeremy Willes <jeremywilles@gmail.com>
2024-02-27 14:03:44 -0500

    api stuff

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,41 +1,25 @@
-import altair as alt
+
 import numpy as np
 import pandas as pd
 import streamlit as st
+import requests
 
-"""
-# Welcome to Streamlit!
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
-'''
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
-
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
-
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
-
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
-
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
-'''
+def fetch_card_templates():
+    try:
+        url = 'https://app.getourdata.com/rest/v1/getourdata/getapi'
+        headers = {
+            'x-api-key': 'Vzg_5NsT6_zv1cLxvG5bSQ',
+            'content-type': 'application/json'
+        }
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()  # Raise an exception for HTTP errors
+        data = response.json()
+        return data
+    except Exception as e:
+        print(e)
+        return []
+    
+card_templates_data = fetch_card_templates()
+card_templates_df = pd.DataFrame(card_templates_data)
+st.write(card_templates_df)
