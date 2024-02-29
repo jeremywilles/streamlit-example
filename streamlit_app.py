afcf4ae65b5664f7c07bffa09bac2ba41c55be7a
Jeremy Willes <jeremywilles@gmail.com>
2024-02-29 14:47:38 -0500

    add list

--- streamlit_app.py
+++ streamlit_app.py
@@ -150,7 +150,7 @@ for val in data:
                 }
             )
             
-            st.button('Add API', on_click=data_insert, args=[val['templateAPIID'], email, password])
+            st.button('Add API', key=val['templateAPIID'] on_click=data_insert, args=[val['templateAPIID'], email, password])
 #             if res:
 #                 st.write(res)
 #                 #st.write(val['templateAPIID'], email, password)
